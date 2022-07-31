2ebe2b0dfc42a30d224fb89479b271b4fc1cd19d
Botprojects <110056159+Botprojects@users.noreply.github.com>
2022-07-31 07:43:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,9 +19,8 @@ def generateQr(msg):
     text = msg.text
     qrcode = segno.make(text)
     qrcode.save('qr.png',scale=3)
-    #read_qr = cv2.imread('qr.png')
     with open('qr.png','rb') as qr:
-        read_qr = cv2.imread(qr,0)
+        read_qr = cv2.imread(qr)
         detector = cv2.QRCodeDetector()
         read,q,r = detector.detectAndDecode(read_qr)
         bot.send_photo(msg.chat.id,qr,reply_to_message_id=msg.message_id)
